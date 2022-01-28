1f64cef40ed2604152b6173911506e9a57e71439
ivan <kulikovia.72@icloud.com>
2022-01-28 04:17:04 +0200

    rename package to Localization

--- Package.swift
+++ Package.swift
@@ -3,13 +3,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "LocalizationManager",
+    name: "Localization",
     platforms: [
         .iOS("15")
     ],
     products: [
-        .library(name: "LocalizationManager", targets: ["LocalizationManager"]),
-        .library(name: "Language", targets: ["Language"])
+        .library(name: "Language", targets: ["Language"]),
+        .library(name: "LocalizationManager", targets: ["LocalizationManager"])
     ],
     dependencies: [
         .package(name: "Core", url: "https://github.com/kutchie-pelaez-packages/Core", .branch("master"))
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# LocalizationManager
+# Localization
